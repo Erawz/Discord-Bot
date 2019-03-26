3c5c4f01e5dad949877033c1a050dad102be9518
Erawz <47424948+Erawz@users.noreply.github.com>
2019-03-26 07:10:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
-client.login("NTQyODczMzI3MTE5NzYxNDEw.Dz0ZJw.VFnLynjLipbOkBX3tkrUqYxNd9s");
+client.login("");
 
 client.commands = new Discord.Collection();
 
